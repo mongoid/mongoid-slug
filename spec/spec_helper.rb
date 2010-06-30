93818f7763a3642f2446d207ce1cbb73c980f167
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-06-30 14:59:10 +0100

    Configure Mongoid before requiring models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,17 @@ require "rubygems"
 require "bundler"
 Bundler.require(:default)
 
-require File.expand_path("../../lib/mongoid_slug", __FILE__)
-Dir["#{File.dirname(__FILE__)}/models/*.rb"].each { |f| require f }
-
 Mongoid.configure do |config|
   name = "mongoid_slug_test"
   host = "localhost"
   config.master = Mongo::Connection.new.db(name)
 end
 
+require File.expand_path("../../lib/mongoid_slug", __FILE__)
+Dir["#{File.dirname(__FILE__)}/models/*.rb"].each { |f| require f }
+
 DatabaseCleaner.orm = "mongoid"
+
 Rspec.configure do |config|
   config.before(:all) do
     DatabaseCleaner.strategy = :truncation
