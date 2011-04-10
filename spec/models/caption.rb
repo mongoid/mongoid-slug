b4e5c257281d0f183d5cb433f4236af184d867c1
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-04-10 20:46:12 +0100

    Add comments.

--- spec/models/caption.rb
+++ spec/models/caption.rb
@@ -4,6 +4,13 @@ class Caption
   field :identity
   field :title
   field :medium
+
+  # A fairly complex scenario, where we want to create a slug out of an
+  # identity field, which comprises name of artist and some more bibliographic
+  # info in parantheses, and the title of the work.
+  #
+  # We are only interested in the name of the artist so we remove the
+  # paranthesized details.
   slug :identity, :title do |doc|
     [doc.identity.gsub(/\s*\([^)]+\)/, ''), doc.title].join(' ')
   end
