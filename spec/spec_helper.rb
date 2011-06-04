640ce23c73faec3c563ceb206f717d4b49d50e0e
dblock <dblock@dblock.org>
2011-06-03 22:12:49 -0400

    Fixed bundler, rspec and yaml complaining on a clean setup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,10 @@ require "mongoid"
 require "stringex"
 require "rspec"
 
+require 'yaml' 
+
+YAML::ENGINE.yamler = 'syck' 
+
 Mongoid.configure do |config|
   name = "mongoid_slug_test"
   config.master = Mongo::Connection.new.db(name)
@@ -15,7 +19,7 @@ require File.expand_path("../../lib/mongoid/slug", __FILE__)
 
 Dir["#{File.dirname(__FILE__)}/models/*.rb"].each { |f| require f }
 
-Rspec.configure do |c|
+RSpec.configure do |c|
   c.before(:all)  { DatabaseCleaner.strategy = :truncation }
   c.before(:each) { DatabaseCleaner.clean }
 end
