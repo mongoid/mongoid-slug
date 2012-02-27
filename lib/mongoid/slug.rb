070e8eef54b43e177c835eab948533049b5a5aa2
Douwe Maan <douwemaan@me.com>
2012-02-27 22:18:01 +0100

    Add ::unique_slug_for and #unique_slug_for methods, returning the unique slug that would be used were the provided string to be used to generate the slug.

--- lib/mongoid/slug.rb
+++ lib/mongoid/slug.rb
@@ -37,9 +37,9 @@ module Mongoid #:nodoc:
       # * `:as`, which specifies name of the field that stores the
       # slug. Defaults to `slug`.
       #
-      # * `:scope`, which specifies a reference association to scope
-      # the slug by. Embedded documents are by default scoped by their
-      # parent.
+      # * `:scope`, which specifies a reference association or field to 
+      # scope the slug by. Embedded documents are by default scoped by 
+      # their parent.
       #
       # * `:reserve`, which specifiees an array of reserved slugs.
       # Defaults to [], the empty array.
@@ -145,6 +145,162 @@ module Mongoid #:nodoc:
           end
         }
       end
+      
+      # Returns the unique slug that would be used, were this string to be
+      # used to generate the slug.
+      # 
+      # Takes a string and an optional hash of options
+      # 
+      # The options hash respects the following members:
+      # 
+      # * `:scope`, which specifies the scope that should be used to generate
+      # the slug, if the class creates scoped slugs. Defaults to `nil`.
+      # * `:model`, which specifies the model that the slug should be 
+      # generated for. This option overrides `:scope` as the scope can now 
+      # be extracted from the model. Defaults to `nil`.
+      def unique_slug_for(slug_to_be, options = {})
+        if slug_scope && self.reflect_on_association(slug_scope).nil?
+          scope_object    = uniqueness_scope(options[:model])
+          scope_attribute = options[:scope] || options[:model].try(:read_attribute, slug_scope)
+        else
+          scope_object = options[:scope] || uniqueness_scope(options[:model])
+          scope_attribute = nil
+        end
+        
+        excluded_id = options[:model]._id if options[:model]
+        
+        slug = slug_to_be.to_url
+        
+        # Regular expression that matches slug, slug-1, ... slug-n
+        # If slug_name field was indexed, MongoDB will utilize that
+        # index to match /^.../ pattern.
+        pattern = /^#{Regexp.escape(slug)}(?:-(\d+))?$/
+
+        if slug_scope &&
+           self.reflect_on_association(slug_scope).nil?
+          # scope is not an association, so it's scoped to a local field
+          # (e.g. an association id in a denormalized db design)
+
+          where_hash = {}
+          where_hash[slug_name]   = pattern
+          where_hash[:_id.ne]     = excluded_id if excluded_id
+          where_hash[slug_scope]  = scope_attribute
+
+          existing_slugs =
+            deepest_document_superclass.
+            only(slug_name).
+            where(where_hash)
+        else
+          where_hash = {}
+          where_hash[slug_name]   = pattern
+          where_hash[:_id.ne]     = excluded_id if excluded_id
+
+          existing_slugs =
+            scope_object.
+            only(slug_name).
+            where(where_hash)
+        end    
+
+        existing_slugs = existing_slugs.map do |doc|
+          doc.read_attribute(slug_name)
+        end
+
+        if slug_history_name
+          if slug_scope &&
+             self.reflect_on_association(slug_scope).nil?
+            # scope is not an association, so it's scoped to a local field
+            # (e.g. an association id in a denormalized db design)
+
+            where_hash = {}
+            where_hash[slug_history_name.all] = [pattern]
+            where_hash[:_id.ne]               = excluded_id if excluded_id
+            where_hash[slug_scope]            = scope_attribute
+
+            history_slugged_documents =
+              deepest_document_superclass.
+              where(where_hash)
+          else
+            where_hash = {}
+            where_hash[slug_history_name.all] = [pattern]
+            where_hash[:_id.ne]               = excluded_id if excluded_id
+
+            history_slugged_documents =
+              scope_object.
+              where(where_hash)
+          end
+
+          existing_history_slugs = []
+          history_slugged_documents.each do |doc|
+            history_slugs = doc.read_attribute(slug_history_name)
+            next if history_slugs.nil?
+            existing_history_slugs.push(*history_slugs.find_all { |slug| slug =~ pattern })
+          end
+
+          # If the only conflict is in the history of a document in the same scope,
+          # transfer the slug
+          if slug_scope && existing_slugs.count == 0 && existing_history_slugs.count > 0
+            history_slugged_documents.each do |doc|
+              doc_history_slugs = doc.read_attribute(slug_history_name)
+              next if doc_history_slugs.nil?
+              doc_history_slugs -= existing_history_slugs
+              doc.write_attribute(slug_history_name, doc_history_slugs)
+              doc.save
+            end
+            existing_history_slugs = []
+          end
+
+          existing_slugs += existing_history_slugs
+        end   
+
+        existing_slugs << slug if slug_reserve.any? { |reserved| reserved === slug }
+
+        if existing_slugs.count > 0
+          # Sort the existing_slugs in increasing order by comparing the
+          # suffix numbers:
+          # slug, slug-1, slug-2, ..., slug-n
+          existing_slugs.sort! do |a, b|
+            (pattern.match(a)[1] || -1).to_i <=>
+            (pattern.match(b)[1] || -1).to_i
+          end
+          max = existing_slugs.last.match(/-(\d+)$/).try(:[], 1).to_i
+
+          slug += "-#{max + 1}"
+        end
+
+        slug
+      end
+      
+      private
+      
+      def uniqueness_scope(model = nil)  
+        if model
+          if slug_scope && (metadata = self.reflect_on_association(slug_scope))
+            parent = model.send(metadata.name)
+
+            # Make sure doc is actually associated with something, and that
+            # some referenced docs have been persisted to the parent
+            #
+            # TODO: we need better reflection for reference associations,
+            # like association_name instead of forcing collection_name here
+            # -- maybe in the forthcoming Mongoid refactorings?
+            inverse = metadata.inverse_of || collection_name
+            return parent.respond_to?(inverse) ? parent.send(inverse) : self
+          end
+          if embedded?
+            parent_metadata = reflect_on_all_associations(:embedded_in)[0]
+            return model._parent.send(parent_metadata.inverse_of || model.metadata.name)
+          end
+        end
+        deepest_document_superclass
+      end
+      
+      def deepest_document_superclass
+        appropriate_class = self
+        while appropriate_class.superclass.include?(Mongoid::Document)
+          appropriate_class = appropriate_class.superclass
+        end
+        appropriate_class
+      end
     end
 
     # Returns the slug.
@@ -155,6 +311,12 @@ module Mongoid #:nodoc:
         read_attribute(slug_name)
       end
     end
+    
+    # Returns the unique slug that would be used, were this string to be
+    # used to generate the slug.
+    def unique_slug_for(slug_to_be)
+      self.class.unique_slug_for(slug_to_be, :model => self)
+    end
 
     private
 
@@ -164,93 +326,12 @@ module Mongoid #:nodoc:
       # Generate a slug only if the slug was not set or changed manually.
       if (new_record? && read_attribute(slug_name).present?) ||
          (!new_record? && slug_field_changed?)
-        slug = read_attribute(slug_name)
+        slug_to_be = read_attribute(slug_name)
       else
-        slug = slug_builder.call(self).to_url
+        slug_to_be = slug_builder.call(self)
       end
 
-      # Regular expression that matches slug, slug-1, ... slug-n
-      # If slug_name field was indexed, MongoDB will utilize that
-      # index to match /^.../ pattern.
-      pattern = /^#{Regexp.escape(slug)}(?:-(\d+))?$/
-
-      if slug_scope &&
-         self.class.reflect_on_association(slug_scope).nil?
-        # scope is not an association, so it's scoped to a local field
-        # (e.g. an association id in a denormalized db design)
-        existing_slugs =
-          self.class.
-          only(slug_name).
-          where(slug_name  => pattern,
-                :_id.ne    => _id,
-                slug_scope => self[slug_scope])
-      else
-        existing_slugs =
-          uniqueness_scope.
-          only(slug_name).
-          where(slug_name => pattern, :_id.ne => _id)
-      end    
-
-      existing_slugs = existing_slugs.map do |obj|
-        obj.read_attribute(slug_name)
-      end
-
-      if slug_history_name
-        if slug_scope &&
-           self.class.reflect_on_association(slug_scope).nil?
-          # scope is not an association, so it's scoped to a local field
-          # (e.g. an association id in a denormalized db design)
-          history_slugged_documents =
-            self.class.
-            where(slug_history_name.all => [pattern],
-                  :_id.ne    => _id,
-                  slug_scope => self[slug_scope])
-        else
-          history_slugged_documents =
-            uniqueness_scope.
-            where(slug_history_name.all => [pattern], 
-                  :_id.ne => _id)
-        end
-
-        existing_history_slugs = []
-        history_slugged_documents.each do |obj|
-          history_slugs = obj.read_attribute(slug_history_name)
-          next if history_slugs.nil?
-          existing_history_slugs.push(*history_slugs.find_all { |slug| slug =~ pattern })
-        end
-
-        # If the only conflict is in the history of a document in the same scope,
-        # transfer the slug
-        if slug_scope && existing_slugs.count == 0 && existing_history_slugs.count > 0
-          history_slugged_documents.each do |doc|
-            doc_history_slugs = doc.read_attribute(slug_history_name)
-            next if doc_history_slugs.nil?
-            doc_history_slugs -= existing_history_slugs
-            doc.write_attribute(slug_history_name, doc_history_slugs)
-            doc.save
-          end
-          existing_history_slugs = []
-        end
-
-        existing_slugs += existing_history_slugs
-      end   
-
-      existing_slugs << slug if slug_reserve.any? { |reserved| reserved === slug }
-
-      if existing_slugs.count > 0
-        # Sort the existing_slugs in increasing order by comparing the
-        # suffix numbers:
-        # slug, slug-1, slug-2, ..., slug-n
-        existing_slugs.sort! do |a, b|
-          (pattern.match(a)[1] || -1).to_i <=>
-          (pattern.match(b)[1] || -1).to_i
-        end
-        max = existing_slugs.last.match(/-(\d+)$/).try(:[], 1).to_i
-
-        slug += "-#{max + 1}"
-      end
-
-      slug
+      unique_slug_for(slug_to_be)
     end
 
     def generate_slug
@@ -269,31 +350,6 @@ module Mongoid #:nodoc:
         history_slugs = read_attribute(slug_history_name) || []
         history_slugs << old_slug
         write_attribute(slug_history_name, history_slugs)
-      end
-    end
-
-    def uniqueness_scope
-      if slug_scope
-        metadata = self.class.reflect_on_association(slug_scope)
-        parent = self.send(metadata.name)
-
-        # Make sure doc is actually associated with something, and that
-        # some referenced docs have been persisted to the parent
-        #
-        # TODO: we need better reflection for reference associations,
-        # like association_name instead of forcing collection_name here
-        # -- maybe in the forthcoming Mongoid refactorings?
-        inverse = metadata.inverse_of || collection_name
-        parent.respond_to?(inverse) ? parent.send(inverse) : self.class
-      elsif embedded?
-        parent_metadata = reflect_on_all_associations(:embedded_in)[0]
-        _parent.send(parent_metadata.inverse_of || self.metadata.name)
-      else
-        appropriate_class = self.class
-        while appropriate_class.superclass.include?(Mongoid::Document)
-          appropriate_class = appropriate_class.superclass
-        end
-        appropriate_class
       end
     end
 
--- spec/mongoid/slug_spec.rb
+++ spec/mongoid/slug_spec.rb
@@ -480,6 +480,35 @@ module Mongoid
       end
     end
 
+    describe "::unique_slug_for" do
+      it "returns the unique slug" do
+        Book.unique_slug_for("A Thousand Plateaus").should eq("a-thousand-plateaus")
+      end
+
+      it "returns the unique slug with a counter if necessary" do
+        Book.create(:title => "A Thousand Plateaus")
+        Book.unique_slug_for("A Thousand Plateaus").should eq("a-thousand-plateaus-1")
+      end
+
+      it "returns the unique slug as if it were the provided object" do
+        book = Book.create(:title => "A Thousand Plateaus")
+        Book.unique_slug_for("A Thousand Plateaus", :model => book).should eq("a-thousand-plateaus")
+      end
+    end
+
+    describe "#unique_slug_for" do
+      let!(:book) { Book.create(:title => "A Thousand Plateaus") }
+
+      it "returns the unique slug" do
+        book.unique_slug_for("Anti Oedipus").should eq("anti-oedipus")
+      end
+
+      it "returns the unique slug with a counter if necessary" do
+        Book.create(:title => "Anti Oedipus")
+        book.unique_slug_for("Anti Oedipus").should eq("anti-oedipus-1")
+      end
+    end
+
     context "when the slugged field is set manually" do
       context "when it set to a non-empty string" do
         it "respects the provided slug" do
