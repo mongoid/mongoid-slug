ad066e6aef4e2ce102ef8d09ba0a715404c161d8
Adam St. John <astjohn@gmail.com>
2012-03-22 08:34:20 -0400

    Finished find slug by id or slug name using #find

--- lib/mongoid/optional.rb
+++ lib/mongoid/optional.rb
@@ -17,10 +17,7 @@ module Mongoid::Criterion::Optional
       # slug
       if ids.size > 1
         if @klass.slug_history_name
-          puts "slug history"
-#          any_of({ @klass.slug_name => ids }, { @klass.slug_history_name => ids })
-#          any_in(@klass.slug_name => ids).also_in(@klass.slug_history_name => ids)
-#          any_in(@klass.slug_name => ids).any_in(@klass.slug_history_name => ids)
+          any_of({ @klass.slug_name.to_sym.in => ids }, { @klass.slug_history_name.to_sym.in => ids })
         else
           where(@klass.slug_name.to_sym.in => ids)
         end
