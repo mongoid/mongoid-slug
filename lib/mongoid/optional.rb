5998e29b8446a5a3c426ce257e9a5ba5f58a92c3
Ben Woosley <ben.woosley@gmail.com>
2012-03-22 19:12:51 -0700

    No good reason not to use a method-level rescue here

--- lib/mongoid/optional.rb
+++ lib/mongoid/optional.rb
@@ -2,30 +2,28 @@ module Mongoid::Criterion::Optional
   # Override Mongoid's finder to use slug or id
   def for_ids(*ids)
     ids.flatten!
-    begin
-      # note that there is a small possibility that a client could create a slug that
-      # resembles a BSON::ObjectId
-      BSON::ObjectId.from_string(ids.first) unless ids.first.is_a?(BSON::ObjectId)
-      # id
-      if ids.size > 1
-        any_in(:_id => ids)
+    # note that there is a small possibility that a client could create a slug that
+    # resembles a BSON::ObjectId
+    BSON::ObjectId.from_string(ids.first) unless ids.first.is_a?(BSON::ObjectId)
+    # id
+    if ids.size > 1
+      any_in(:_id => ids)
+    else
+      where(:_id => ids.first)
+    end
+  rescue BSON::InvalidObjectId
+    # slug
+    if ids.size > 1
+      if @klass.slug_history_name
+        any_of({ @klass.slug_name.to_sym.in => ids }, { @klass.slug_history_name.to_sym.in => ids })
       else
-        where(:_id => ids.first)
+        where(@klass.slug_name.to_sym.in => ids)
       end
-    rescue BSON::InvalidObjectId
-      # slug
-      if ids.size > 1
-        if @klass.slug_history_name
-          any_of({ @klass.slug_name.to_sym.in => ids }, { @klass.slug_history_name.to_sym.in => ids })
-        else
-          where(@klass.slug_name.to_sym.in => ids)
-        end
+    else
+      if @klass.slug_history_name
+        any_of({@klass.slug_name => ids.first}, {@klass.slug_history_name => ids.first})
       else
-        if @klass.slug_history_name
-          any_of({@klass.slug_name => ids.first}, {@klass.slug_history_name => ids.first})
-        else
-          where(@klass.slug_name => ids.first)
-        end
+        where(@klass.slug_name => ids.first)
       end
     end
   end
