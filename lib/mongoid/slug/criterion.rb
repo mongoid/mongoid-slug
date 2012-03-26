6b60ccc67a3b838259dee730f80b9ee05e8b96c1
Ben Woosley <ben.woosley@gmail.com>
2012-03-26 15:41:08 -0700

    Rescue outside the scope of the first super so we don't apply the rescue to non-Mongoid::Slug classes

--- lib/mongoid/slug/criterion.rb
+++ lib/mongoid/slug/criterion.rb
@@ -3,24 +3,26 @@ module Mongoid::Slug::Criterion
   def for_ids(*ids)
     return super unless @klass.ancestors.include?(Mongoid::Slug)
 
-    # note that there is a small possibility that a client could create a slug that
-    # resembles a BSON::ObjectId
-    ids.flatten!
-    BSON::ObjectId.from_string(ids.first) unless ids.first.is_a?(BSON::ObjectId)
-    super # Fallback to original Mongoid::Criterion::Optional
-  rescue BSON::InvalidObjectId
-    # slug
-    if ids.size > 1
-      if @klass.slug_history_name
-        any_of({ @klass.slug_name.to_sym.in => ids }, { @klass.slug_history_name.to_sym.in => ids })
+    begin
+      # note that there is a small possibility that a client could create a slug that
+      # resembles a BSON::ObjectId
+      ids.flatten!
+      BSON::ObjectId.from_string(ids.first) unless ids.first.is_a?(BSON::ObjectId)
+      super # Fallback to original Mongoid::Criterion::Optional
+    rescue BSON::InvalidObjectId
+      # slug
+      if ids.size > 1
+        if @klass.slug_history_name
+          any_of({ @klass.slug_name.to_sym.in => ids }, { @klass.slug_history_name.to_sym.in => ids })
+        else
+          where(@klass.slug_name.to_sym.in => ids)
+        end
       else
-        where(@klass.slug_name.to_sym.in => ids)
-      end
-    else
-      if @klass.slug_history_name
-        any_of({@klass.slug_name => ids.first}, {@klass.slug_history_name => ids.first})
-      else
-        where(@klass.slug_name => ids.first)
+        if @klass.slug_history_name
+          any_of({@klass.slug_name => ids.first}, {@klass.slug_history_name => ids.first})
+        else
+          where(@klass.slug_name => ids.first)
+        end
       end
     end
   end
