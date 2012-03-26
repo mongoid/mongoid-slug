c758c5bed40e12108e60ef68afc895d23b2b1ab6
Ben Woosley <ben.woosley@gmail.com>
2012-03-26 11:18:46 -0700

    Add an explanatory comment

--- lib/mongoid/slug/criterion.rb
+++ lib/mongoid/slug/criterion.rb
@@ -3,6 +3,9 @@ module Mongoid::Slug::Criterion
   def for_ids(*ids)
     return super unless @klass.ancestors.include?(Mongoid::Slug)
 
+    # We definitely don't want to rescue at the same level we call super above -
+    # that would risk applying our slug behavior to non-slug objects, in the case
+    # where their id conversion fails and super raises BSON::InvalidObjectId
     begin
       # note that there is a small possibility that a client could create a slug that
       # resembles a BSON::ObjectId
