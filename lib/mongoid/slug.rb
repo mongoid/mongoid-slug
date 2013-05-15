dac6613736376575b16de70a2383653d5f767659
s12chung <steve.chung7@gmail.com>
2013-05-15 19:41:42 -0400

    added synicing option

--- lib/mongoid/slug.rb
+++ lib/mongoid/slug.rb
@@ -9,7 +9,9 @@ module Mongoid
                      :slugged_attributes,
                      :url_builder,
                      :history,
-                     :by_model_type
+                     :by_model_type,
+                     :sync,
+                     :syncing
 
       # field :_slugs, type: Array, default: [], localize: false
       # alias_attribute :slugs, :_slugs
@@ -55,6 +57,7 @@ module Mongoid
         self.slugged_attributes    = fields.map &:to_s
         self.history               = options[:history]
         self.by_model_type         = options[:by_model_type]
+        self.sync                  = options[:sync]
 
         field :_slugs, type: Array, default: [], localize: options[:localize]
         alias_attribute :slugs, :_slugs
@@ -144,6 +147,14 @@ module Mongoid
         self._slugs = [_new_slug]
       end
 
+      if sync
+        self.syncing = true
+        slugged_attributes.each do |slugged_attribute|
+          update_attribute slugged_attribute, _new_slug
+        end
+        self.syncing = false
+      end
+
       true
 
     end
@@ -160,7 +171,7 @@ module Mongoid
 
     # @return [Boolean] Whether the slug requires to be rebuilt
     def slug_should_be_rebuilt?
-      new_record? or _slugs_changed? or slugged_attributes_changed?
+      (new_record? or _slugs_changed? or slugged_attributes_changed?) and !syncing
     end
 
     def slugged_attributes_changed?
